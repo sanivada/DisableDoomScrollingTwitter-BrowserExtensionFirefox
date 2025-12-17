2d38fdda309eb992feca2bfe3d566657b4ace13d
Chaitanya Sanivada <chaitanysanivada@gmail.com>
2025-12-17 18:40:59 +0530

    hide new tweets after limit is reached

--- limitPosts.js
+++ limitPosts.js
@@ -1,4 +1,29 @@
 let visibleTweetCounter = 0;
+let LIMIT = 20;
+let limitReached = false;
+
+function cleanUpFeed(lastVisibleTweet) {
+
+    let sibling = lastVisibleTweet.closest('div[data-testid="cellInnerDiv"]').nextElementSibling;
+
+    while (sibling) {
+        sibling.style.display = 'none';
+        sibling = sibling.nextElementSibling;
+    }
+    console.log('cleaned the tweets after N tweets, after the limit is reached.')
+
+    // add a stop sign
+    const stopSign = document.createElement('div');
+    stopSign.innerText = `You have reached your limit of ${LIMIT} tweets`;
+    stopSign.style.fontWeight = "bold";
+    stopSign.style.textAlign = "center";
+    stopSign.style.padding = "20px";
+
+    const tweetContainer = document.querySelector('div[aria-label="Home timeline"]');
+    tweetContainer.append(stopSign);
+    // lastVisibleTweet.parentNode.appendChild(stopSign);
+    console.log('added the stop sign!')
+};
 
 function startApp(feedContainer) {
 
@@ -8,6 +33,16 @@ function startApp(feedContainer) {
             if (entry.isIntersecting) {
                 visibleTweetCounter++;
                 console.log(`counted a tweet! total: ${visibleTweetCounter}`);
+                
+                if (visibleTweetCounter >= LIMIT) {
+                    limitReached = true;
+                    console.log('LIMIT REACHED.');
+                    // clean up rest of tweets in the dom
+                    observer.disconnect();
+                    cleanUpFeed(entry.target);
+                };
+
+
                 // entry.target.setAttribute('data-seen', 'true');
                 observer.unobserve(entry.target);
             }
@@ -36,14 +71,22 @@ function startApp(feedContainer) {
             if (mutation.type == 'childList') {
                 mutation.addedNodes.forEach(node => {
                     if (node.nodeType === 1) {
-                        if (node.matches('article[data-testid="tweet"]')) {
-                            tweetObserver.observe(node);
-                        }
 
-                        const newTweets = node.querySelectorAll('article[data-testid="tweet"]');
-                        newTweets.forEach(tweet => {
-                            tweetObserver.observe(tweet);
-                        });
+                        if (limitReached) {
+                            if (node.matches('article[data-testid="tweet"]') ||
+                                    node.querySelector('article[data-testid="tweet"]')) {
+                                        node.style.display = 'none';
+                                    };
+                        } else {
+                            if (node.matches('article[data-testid="tweet"]')) {
+                                tweetObserver.observe(node);
+                            }
+
+                            const newTweets = node.querySelectorAll('article[data-testid="tweet"]');
+                            newTweets.forEach(tweet => {
+                                tweetObserver.observe(tweet);
+                            });
+                        }
                     }
                 });
             }
